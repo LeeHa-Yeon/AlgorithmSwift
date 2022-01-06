49e6a79f78ad649279a4e8ee1f63a83fef1f0603
이하연 <gkdus1613@naver.com>
2022-01-06 22:27:06 +0900

    [BaekJun] 1000_A+B

---
+++ Baekjoon/입출력과사칙연산/1000_A+B.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let inputValue = readLine()
+
+if let inputResult = inputValue {
+    let array = inputResult.components(separatedBy: " ")
+    let answer = Int(array[0])! + Int(array[1])!
+    print(answer)
+}
