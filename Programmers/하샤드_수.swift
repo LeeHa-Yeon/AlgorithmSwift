78066dfde34511bfffa72a877006fd673c549a5f
이하연 <gkdus1613@naver.com>
2022-01-31 23:12:41 +0900

    Programmers [ 1Level ] - 하샤드수 re

---
+++ Programmers/하샤드_수.swift
@@ -0,0 +1,14 @@
+func solution(_ x:Int) -> Bool {
+    let arr = String(x).map{ Int(String($0))! }
+    var total: Int = 0
+    
+    for value in arr {
+        total += value
+    }
+    
+    if x%total == 0 {
+        return true
+    }
+    
+    return false
+}
