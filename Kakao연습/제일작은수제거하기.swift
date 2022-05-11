1c5c12c258ab396764ff333f39ad4c58df6b432f
이하연 <gkdus1613@naver.com>
2022-05-11 17:04:46 +0900

    Update 제일작은수제거하기.swift

--- Kakao연습/제일작은수제거하기.swift
+++ Kakao연습/제일작은수제거하기.swift
@@ -1,6 +1,20 @@
+
+// 시간초과 : 1번 테케
 import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
     var newArr = arr.filter{ $0 != arr.min()! }
     return newArr == [] ? [-1] : newArr
+}
+
+// 모두 통과
+import Foundation
+
+func solution(_ arr:[Int]) -> [Int] {
+    var arr = arr
+    var minValue = arr.min()!
+    var findIdx = arr.index(of: minValue)!
+    arr.remove(at: findIdx)
+    
+    return arr == [] ? [-1] : arr
 }
