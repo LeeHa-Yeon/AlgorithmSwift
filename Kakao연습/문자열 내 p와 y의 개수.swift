a633c44266b01e67fecd69b5f43ca49723edecf9
이하연 <gkdus1613@naver.com>
2022-05-13 00:17:52 +0900

    Update 문자열 내 p와 y의 개수.swift

--- Kakao연습/문자열 내 p와 y의 개수.swift
+++ Kakao연습/문자열 내 p와 y의 개수.swift
@@ -9,10 +9,19 @@ func solution(_ s:String) -> Bool
 
 
 import Foundation
-
+ // 하나 안됬음
 func solution(_ s:String) -> Bool
 {
     var pCnt = Array(s).filter { $0 == "p" || $0 == "P" }.count
     var yCnt = Array(s).filter { $0 == "y" || $0 == "Y" }.count
     return pCnt == yCnt ? true : false
+}
+
+import Foundation
+ // 통과
+func solution(_ s:String) -> Bool
+{
+    var pCnt = s.filter { $0 == "p" || $0 == "P" }.count
+    var yCnt = s.filter { $0 == "y" || $0 == "Y" }.count
+    return pCnt == yCnt
 }
