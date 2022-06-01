ecbaad29999b068d4b95ae13947b4b6cc456cda2
이하연 <gkdus1613@naver.com>
2022-06-01 13:03:50 +0900

    Create 핸드폰 번호 가리기.swift

---
+++ ReTry/핸드폰 번호 가리기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ phone_number:String) -> String {
+    var answer: String = ""
+    
+    for i in 0..<phone_number.count-4 {
+        answer += "*"
+    }
+    
+    return answer + phone_number.suffix(4)
+    
+}
