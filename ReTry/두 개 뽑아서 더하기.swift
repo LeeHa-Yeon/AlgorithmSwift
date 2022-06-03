20fc75fe8dae0b1abe8582a0ce65323e5530ce87
이하연 <gkdus1613@naver.com>
2022-06-03 22:13:55 +0900

    Create 두 개 뽑아서 더하기.swift

---
+++ ReTry/두 개 뽑아서 더하기.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result = Set<Int>()
+    getCombination(numbers,2,0,&result,[])
+    
+    return result.sorted{ $0 < $1 }
+}
+
+func getCombination(_ arr: [Int], _ num: Int, _ idx: Int = 0, _ res: inout Set<Int>, _ tmp: [Int]) {
+    if tmp.count == num {
+        res.insert(tmp.reduce(0,+))
+        return 
+    }
+    
+    for i in idx..<arr.count {
+        getCombination(arr,num,i+1,&res,tmp+[arr[i]])
+    }
+}
