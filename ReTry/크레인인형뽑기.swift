f0ebeaf76e493db7d2f471df5fc5675c3ab450e5
이하연 <gkdus1613@naver.com>
2022-06-07 03:35:24 +0900

    Update 크레인인형뽑기.swift

--- ReTry/크레인인형뽑기.swift
+++ ReTry/크레인인형뽑기.swift
@@ -6,7 +6,7 @@ func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
     var stack: [Int] = []
     var answer: Int = 0
     
-    for (idx,move) in moves.enumerated() {
+    for move in moves {
         for i in 0..<board.count {
             if board[i][move-1] != 0 {
                 stack.append(board[i][move-1])
@@ -15,11 +15,9 @@ func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
             }
         }
         
-        if stack.count >= 2 {
-            if stack[stack.count-2] == stack[stack.count-1] {
-                answer += 2
-                stack = Array(stack.prefix(stack.count-2))
-            }
+        if stack.count >= 2 && (stack[stack.count-2] == stack[stack.count-1]) {
+            answer += 2
+            stack = Array(stack.prefix(stack.count-2))
         }
     }
     
