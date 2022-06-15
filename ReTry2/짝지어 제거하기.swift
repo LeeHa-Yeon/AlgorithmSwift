c7d9ad5865ab8a543ed17e459f3730320df904dd
이하연 <gkdus1613@naver.com>
2022-06-15 23:45:32 +0900

    Update 짝지어 제거하기.swift

--- ReTry2/짝지어 제거하기.swift
+++ ReTry2/짝지어 제거하기.swift
@@ -2,7 +2,6 @@ import Foundation
 
 func solution(_ s:String) -> Int{
     var stack: [Character] = []
-    var current: Character = s[s.startIndex]
     
     for c in s {
         if stack.count > 0 && stack.last! == c {
