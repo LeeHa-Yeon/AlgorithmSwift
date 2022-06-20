ce22814adc05da7b87bdf10b55bbc9772ea86b19
이하연 <gkdus1613@naver.com>
2022-06-20 17:33:27 +0900

    Update n^2 배열 자르기.swift

--- ReTry2/n^2 배열 자르기.swift
+++ ReTry2/n^2 배열 자르기.swift
@@ -4,11 +4,13 @@ func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
     var answer: [Int] = []
     
     for location in Int(left)...Int(right) {
-        let row = Int(location/n)
-        let col = Int(location%n)
+        let row = Int(location/n) // 나누기 -> 행
+        let col = Int(location%n) // 나머지 -> 열
         if col <= row {
+            // 열 <= 행 -> 해당 값은 행 + 1
             answer.append(row+1)
         }else {
+            // 행 < 열 -> 해당 값은 열 + 1
             answer.append(col+1)
         }
     }
