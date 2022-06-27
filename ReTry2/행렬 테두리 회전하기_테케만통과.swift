48782e2ae623671c29cfceb3856c91ca877eab23
이하연 <gkdus1613@naver.com>
2022-06-27 18:13:53 +0900

    Create 행렬 테두리 회전하기_테케만통과.swift

---
+++ ReTry2/행렬 테두리 회전하기_테케만통과.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+func solution(_ rows:Int, _ columns:Int, _ queries:[[Int]]) -> [Int] {
+    var arr: [[Int]] = []
+    var answer: [Int] = []
+    
+    // 행렬판 초기화
+    settingArr(rows,columns,&arr)
+    
+    // 좌표 이동
+    for query in queries {
+        let r1 = query[0], c1 = query[1], r2 = query[2],c2 = query[3]
+        var temp = arr[r1-1][c1-1]
+        var minValue = arr[rows-1][columns-1]
+        minValue = min(minValue,temp)
+        
+        // 왼쪽
+        for i in r1-1..<r2-1 {
+            let temp1 = arr[i+1][c1-1]
+            arr[i][c1-1] = temp1
+            minValue = min(minValue,temp1)
+        }
+        
+        // 아래
+        for i in c1-1..<c2-1 {
+            let temp2 = arr[r2-1][i+1]
+            arr[r2-1][i] = temp2
+            minValue = min(minValue,temp2)
+        }
+        
+        
+        // 오른쪽
+        for i in stride(from: r2-1 , to: r1-1, by: -1) {
+            let temp3 = arr[i-1][c2-1]
+            arr[i][c2-1] = temp3
+            minValue = min(minValue,temp3)
+        }
+        
+        // 위
+        for i in stride(from: c2-1 , to: c1-1, by: -1) {
+            let temp4 = arr[r1-1][i-1]
+            arr[r1-1][i] = temp4
+            minValue = min(minValue,temp4)
+        }
+        arr[r1-1][c1] = temp
+        answer.append(minValue)
+    }
+    
+    return answer
+}
+
+func settingArr(_ rows:Int, _ columns:Int, _ result: inout [[Int]]) { 
+    var i: Int = 1
+    for _ in 1...rows {
+        result.append(Array(i..<i+columns))
+        i += columns
+    }
+    
+}
